201420183f5f933af9a5c4b27889bdbd0ce0ff27
Alain Vaucher <avaucher@student.ethz.ch>
2014-04-07 19:25:58 +0200

    ...

--- code/main.m
+++ code/main.m
@@ -1,18 +1,31 @@
+function dy = ode(t,y)
+    global M N
+    M
+    N
+    
+end
+
+
+
 %---------------------------------
 % Initialization of the variables
 
+    global N
+    N = 5; %Number of bees
+    global M 
+    M = 2; %Number of tasks
 
-N = 5; %Number of bees
-M = 2; %Number of tasks
+    x = ones(N,M)*0.1;
+    theta = ones(N,M)*500;
+    s  = ones(1,M);
 
-x = ones(N,M)*0.1;
-theta = ones(N,M)*500;
-s  = ones(1,M);
-
-alpha = 3;
-delta = 1;
-p=0.2;
-zeta=10;
-phi=1;
-sigma=0.1;
-dt=1;
+    alpha = 3;
+    delta = 1;
+    p=0.2;
+    zeta=10;
+    phi=1;
+    sigma=0.1;
+    dt=1
+    
+    disp(dt)
+    dy
