f8e1def04a020560f1ca7f046e7385caf929721b
sjulien <sjulien@student.ethz.ch>
2014-04-09 18:23:49 +0200

    function dy (part 2)

--- code/main.m
+++ code/main.m
@@ -2,7 +2,7 @@
 %---------------------------------
 % Initialization of the variables
 function main
-    global N N phi zeta theta_min theta_max delta alpha
+    global N N phi zeta theta_min theta_max delta alpha p
     'start'
     N = 5; %Number of bees
     M = 2; %Number of tasks
@@ -12,6 +12,7 @@ function main
     s  = ones(1,M);
 
     initial_condition = [reshape(theta, N*M,1); reshape(x, N*M,1);reshape(s,M,1)]; %intial condition for the ode_solver
+    initial_condition
     
     alpha = 3;
     delta = 1;
@@ -21,44 +22,49 @@ function main
     sigma=0.1;
     dt=1;
     
-    theta_min=0; %we need to set theta_min
-    theta_max=1000; %we need to set theta_max
+    theta_min=1; 
+    theta_max=1000; 
     
-    disp(dt)
+ 
     ode(1,initial_condition)
 end
 
 
 function dy = ode(t,y)
-    global M N phi zeta theta_min theta_max delta alpha
+    global M N phi zeta theta_min theta_max delta alpha p
     dy = zeros(2*N*M+M,1);
     
     %theta matrix
-    for i=1:M
-        for j=1:N
+    for j=1:M
+        for i=1:N
             factor = 0;
-            y((i-1)*N + j, 1)
-            if (y((i-1)*N + j, 1) > theta_min) && (y((i-1)*N + j, 1)<theta_max)
+            y((j-1)*N + i, 1)
+            if (y((j-1)*N + i, 1) > theta_min) && (y((j-1)*N + i, 1)<theta_max)
                 factor = 1;
             end 
             
-            dy((i-1)*N + j, 1) = [(1 - y((i-1)*N + j +N*M, 1))*phi- y((i-1)*N + j +N*M, 1)*zeta]*factor;
+            dy((j-1)*N + i, 1) = [(1 - y((j-1)*N + i +N*M, 1))*phi- y((j-1)*N + i +N*M, 1)*zeta]*factor;
             %xij: dy((i-1)*N + j +N*M, 1)  
         end
     end
     
     %x matrix
-    for i=1:M
-        for j=1:N
-            dy(M*N+(i-1)*N + j, 1) = 0.6;
+    for j=1:M
+        for i=1:N
+            sum = 0;
+            for w=1:M
+                sum = sum+y(M*N+(w-1)*N + i);
+            end
+            gaussian_sp = 0;
+            dy(M*N+(j-1)*N + i, 1) = [y(M*N*2+j)^2/(y(M*N*2+j)^2+y((j-1)*N + i)^2)]*[1-sum]-p*y(M*N+(j-1)*N + i, 1)+gaussian_sp;
         end
     end 
     
     %s matrix
-    for i=1:M
-        dy(2*M*N+i) = dy(2*M*N+i) + delta;
-        for j=1:N
-            dy(2*M*N+i)= dy(2*M*N+i) - alpha/N*y(M*N+(i-1)*N+j);
+    for j=1:M
+        dy(2*M*N+j) = dy(2*M*N+j) + delta;
+        for i=1:N
+            dy(2*M*N+j)= dy(2*M*N+j) - alpha/N*y(M*N+(j-1)*N+i);
         end
     end
 
