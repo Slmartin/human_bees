b088181f5cf211e9ed6d604361961f7ff68eebbe
sjulien <sjulien@student.ethz.ch>
2014-04-10 12:10:45 +0200

    Diff. Equation (version 1) & plots

--- code/main.m
+++ code/main.m
@@ -24,11 +24,31 @@ function main
     theta_min=1; 
     theta_max=1000; 
     
-    [T,Y]=ode23(@ode_function, [0 3000], initial_condition);
+    %[T,Y]=ode45(@ode_function, [0 3000], initial_condition);
+    [T,Y]=euler_method(@ode_function, [0 3000], initial_condition,0.5);
     
+
     %plot(T,Y(:,1:M*N),'-o')
     %plot(T,Y(:,13),'-r')
-    plot(T,Y(:,1:M*N),'-o')
+    subplot(2,2,1)
+    plot(T,Y(1:M*N,:),'-o')
+    axis([0 3000 0 1100])
+    title('theta')
+    
+    subplot(2,2,2)
+    plot(T,Y(M*N+1:M*N*2,:),'-')
+    axis([0 3000 0 1.1])
+    title('x')
+    
+    subplot(2,2,3)
+    plot(T,Y(M*N*2+1:M*N*2+M,:),'-')
+    title('s')
+    
+    %plot(T,Y(M*N+1,:),'-')
+    %axis([0 3000 0 1.1])
+    subplot(2,2,4)
+    plot(T,Y(M*N+2,:),'-')
+    axis([0 3000 0 1.1])
     %%we have no gaussian process there all bees behave the same way
     %%
     %But it work !!!! 
@@ -38,12 +58,12 @@ end
 function dy = ode_function(t,y)
     global M N phi zeta theta_min theta_max delta alpha p sigma
     dy = zeros(2*N*M+M,1);
-
+    
     %theta matrix
     for j=1:M
         for i=1:N
             factor = 0;
-            y((j-1)*N + i, 1)
+            y((j-1)*N + i, 1);
             if (y((j-1)*N + i, 1) > theta_min) && (y((j-1)*N + i, 1)<theta_max)
                 factor = 1;
             end 
@@ -63,13 +83,14 @@ function dy = ode_function(t,y)
             %gaussian_sp = 0;
             %here is a attempt to get the gaussian centerd process
             %unfortunately the gaussian random number generator seems to be
-            %to slow!!!
-            gaussian_sp = normrnd(0,sigma);
+            %way to slow!!!
+            %%%gaussian_sp = normrnd(0,sigma);
+            gaussian_sp = sigma*randn;
             
             dy(M*N+(j-1)*N + i, 1) = [y(M*N*2+j)^2/(y(M*N*2+j)^2+y((j-1)*N + i)^2)]*[1-sum]-p*y(M*N+(j-1)*N + i, 1)+gaussian_sp;
         end
     end 
-    
+  
     %s matrix
     for j=1:M
         dy(2*M*N+j) = dy(2*M*N+j) + delta;
@@ -82,4 +103,26 @@ function dy = ode_function(t,y)
 end
 
 
+function [T,Y] = euler_method(fun,time, y0, h)
 
+    T = [];
+    Y = [];
+    T = [T,time(1)];
+    Y = [Y,y0];
+    
+    
+    y_eul = y0;
+    t_eul = time(1);
+    
+    n = (time(2)-time(1))/h;
+    
+    for j=1:n
+        y_eul = y_eul + h*fun(t_eul,y_eul);
+        t_eul = t_eul + h ;
+        
+        Y = [Y, y_eul];
+        T = [T, t_eul];
+    end
+    
+
+end 
