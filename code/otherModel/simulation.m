932fa50b6fed89c68413a3bd241b13adbe8efce4
Alain Vaucher <avaucher@student.ethz.ch>
2014-04-23 12:02:35 +0200

    Added boredom in alternative model

--- code/otherModel/simulation.m
+++ code/otherModel/simulation.m
@@ -2,21 +2,27 @@
 clearvars;
 
 %Initialize the variables:
-N=5; %Number of persons
+N=7; %Number of persons
 M=3; %Number of tasks
 
 taskvalue = ones(1,M);
 chosenTask = zeros(N,1);
 productivity = 5 + ones(N,M);% randn(N,M);
+boredom = zeros(N,M);
+boredomAtChosenTask = zeros(N,1);
 production = zeros(1,M);
 money = zeros(N,1);
 learning = 0.01;
 forgetting = 0;
+boredomIncrease = 0.001;
+boredomDecrease = 0.0005;
 
 %Variables for simulation
-timesteps=50;
+timesteps=500;
 stepSize=1;
-output = zeros(N,1);
+taskTime = zeros(N,1);
+moneyTime = zeros(N,1);
+boredomTime = zeros(N,1);
 
 %initialize choice: everyone choses task where productivity is best
 maxProductivity = zeros(N,1);
@@ -46,29 +52,58 @@ for t=0:timesteps
         for j=1:M
             if(chosenTask(i,1) == j)
                 productivity(i,j) = min(10, productivity(i,j) + learning);
+                boredom(i,j) = min(9, boredom(i,j) + boredomIncrease);
             else
-                productivity(i,j) = max(1, productivity(i,j) - forgetting);
+                productivity(i,j) = max(4, productivity(i,j) - forgetting);
+                boredom(i,j) = max(0, boredom(i,j) - boredomDecrease);
             end
         end
     end
     
     %Allow one person to change the job
-    theChosenOne = mod(t,N) + 1;
-    maxGain=0;
-    bestTask=0;
-    for j=1:M
-        if(j == chosenTask(theChosenOne,1))
-            gain = productivity(theChosenOne,j) / production(chosenTask(theChosenOne,1));
-        else
-            gain = productivity(theChosenOne,j) / (production(1,j)+productivity(theChosenOne,j));
+    if(mod(t,10) == 0)
+        theChosenOne = mod(t/10,N) + 1;
+        maxGain=0;
+        bestTask=0;
+        for j=1:M
+            if(j == chosenTask(theChosenOne,1))
+                gain = productivity(theChosenOne,j) / production(chosenTask(theChosenOne,1));
+            else
+                gain = productivity(theChosenOne,j) / (production(1,j)+productivity(theChosenOne,j));
+            end
+            gain = gain - boredom(theChosenOne,j);
+            if(gain > maxGain)
+                maxGain=gain;
+                bestTask=j;
+            end
+            %[theChosenOne j gain]
         end
-        if(gain > maxGain)
-            maxGain=gain;
-            bestTask=j;
-        end
-        [theChosenOne j gain]
+        chosenTask(theChosenOne,1) = bestTask;
     end
-    chosenTask(theChosenOne,1) = bestTask;
-    output = [output chosenTask];
+    
+    %Update boredom
+    for i=1:N
+        boredomAtChosenTask(i,1) = boredom(i,chosenTask(i,1));
+    end
+    
+    taskTime = [taskTime chosenTask];
+    moneyTime = [moneyTime money];
+    boredomTime = [boredomTime boredomAtChosenTask];
 end
 
+time = 0:timesteps+1;
+
+subplot(2,2,1)
+plot(time, taskTime)
+axis([0 timesteps 0 M+1])
+title('Chosen tasks');
+
+subplot(2,2,2)
+plot(time, moneyTime)
+axis([0 timesteps 0 1.1])
+title('Money earned');
+
+subplot(2,2,3)
+plot(time, boredomTime)
+axis([0 timesteps 0 1])
+title('Boredom at chosen task');
