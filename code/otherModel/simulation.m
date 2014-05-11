29dc744a5810a7d50832f40626c7d41491bb7aef
Alain Vaucher <avaucher@student.ethz.ch>
2014-05-11 18:42:08 +0200

    added possibility to kill someone

--- code/otherModel/simulation.m
+++ code/otherModel/simulation.m
@@ -2,23 +2,35 @@
 clearvars;
 
 %Initialize the variables:
-N=8; %Number of persons
-M=4; %Number of tasks
+N=7; %Number of persons
+M=3; %Number of tasks
 
 %Parameters of the model
-abilities = ones(N,1) + abs(randn(N,1));
-%boredomSensitivity = abs(1+randn(N,1)/2.5);
-initialProductivity = 1.0+abs(4*randn(N,M));
-maximalBoredom = (1.0+abs(4*randn(N,M)))*0.15;
+abilitymu = 1;
+abilitysd = 1;
+prodmu = 1;
+prodsd = 4;
+boredommu=0.10;
+boredomsd = 0.60;
 learning = 0.01;
 forgetting = 0.003;
-boredomIncrease = 0.01;
+boredomIncrease = 0.001;
 boredomDecrease = 0.0005;
 switchPossibilityFrequency = 0.01; %Meaning ~all 1/x days
+
+%initialize random variables
+abilities = abilitymu + abilitysd*abs(randn(N,1));
+initialProductivity = prodmu + prodsd*abs(randn(N,M));
+maximalBoredom = boredommu + boredomsd*abs(randn(N,M));
+
 
 %Variables for simulation
 simulationTime=10000;
 dt = 1;
+
+catastrophe = false;
+catastropheTime = 3000;
+
 
 %Other needed variables
 taskvalue = ones(1,M);
@@ -37,6 +49,7 @@ boredomTime = zeros(N,1);
 productivityTime = zeros(N,1);
 totalProduction = 0;
 productionTime = 0;
+alive = ones(N,1);
 
 %Initialize dependent values
 maximalProductivity = initialProductivity;
@@ -63,62 +76,87 @@ for t=0:timesteps
     %Update total production
     production = zeros(1,M);
     for i=1:N
-        production(chosenTask(i,1)) =  production(chosenTask(i,1)) + productivity(i,chosenTask(i,1));
+        if(alive(i,1))
+            production(chosenTask(i,1)) =  production(chosenTask(i,1)) + productivity(i,chosenTask(i,1));
+        end
     end
     
     %Calculate who earns how much money and update total production
     totalProduction = 0;
     for i=1:N
-        money(i,1) = productivity(i,chosenTask(i,1)) / production(chosenTask(i,1));
-        totalmoney(i,1) = totalmoney(i,1) + money(i,1);
-        totalProduction = totalProduction + productivity(i,chosenTask(i,1));
+        if(alive(i,1))
+            money(i,1) = productivity(i,chosenTask(i,1)) / production(chosenTask(i,1));
+            totalmoney(i,1) = totalmoney(i,1) + money(i,1);
+            totalProduction = totalProduction + productivity(i,chosenTask(i,1));
+        end
     end
     
     %Update abilities
     for i=1:N
-        for j=1:M
-            if(chosenTask(i,1) == j)
-                productivity(i,j) = productivity(i,j) + (maximalProductivity(i,j)-productivity(i,j))*(learning*dt);
-                %productivity(i,j) = min(maximalProductivity(i,j), productivity(i,j) + learning*dt);
-                boredom(i,j) = boredom(i,j) + (maximalBoredom(i,j)-boredom(i,j))*(boredomIncrease*dt);
-                %boredom(i,j) = boredom(i,j) + (maximalBoredom(i,j)-boredom(i,j))*(boredomIncrease*boredomSensitivity(i,1)*dt);
-                %boredom(i,j) = min(maximalBoredom(i,j), boredom(i,j) + boredomIncrease*boredomSensitivity(i,1)*dt);
-            else
-                productivity(i,j) = productivity(i,j) - (productivity(i,j))*(forgetting*dt);
-                %productivity(i,j) = max(initialProductivity(i,j), productivity(i,j) - forgetting*dt);
-                boredom(i,j) = boredom(i,j) - (boredom(i,j))*(boredomDecrease*dt);
-                %boredom(i,j) = boredom(i,j) - (boredom(i,j))*(boredomDecrease*boredomSensitivity(i,1)*dt);
-                %boredom(i,j) = max(0, boredom(i,j) - boredomDecrease*dt);
+        if(alive(i,1))
+            for j=1:M
+                if(chosenTask(i,1) == j)
+                    productivity(i,j) = productivity(i,j) + (maximalProductivity(i,j)-productivity(i,j))*(learning*dt);
+                    %productivity(i,j) = min(maximalProductivity(i,j), productivity(i,j) + learning*dt);
+                    boredom(i,j) = boredom(i,j) + (maximalBoredom(i,j)-boredom(i,j))*(boredomIncrease*dt);
+                    %boredom(i,j) = boredom(i,j) + (maximalBoredom(i,j)-boredom(i,j))*(boredomIncrease*boredomSensitivity(i,1)*dt);
+                    %boredom(i,j) = min(maximalBoredom(i,j), boredom(i,j) + boredomIncrease*boredomSensitivity(i,1)*dt);
+                else
+                    productivity(i,j) = productivity(i,j) - (productivity(i,j)-initialProductivity(i,j))*(forgetting*dt);
+                    %productivity(i,j) = max(initialProductivity(i,j), productivity(i,j) - forgetting*dt);
+                    boredom(i,j) = boredom(i,j) - (boredom(i,j))*(boredomDecrease*dt);
+                    %boredom(i,j) = boredom(i,j) - (boredom(i,j))*(boredomDecrease*boredomSensitivity(i,1)*dt);
+                    %boredom(i,j) = max(0, boredom(i,j) - boredomDecrease*dt);
+                end
             end
         end
     end
     
     %Allow one person to change the job
     for i=1:N
-        if(rand() < switchPossibilityFrequency*dt)
-            maxGain=-1000;
-            bestTask=0;
-            for j=1:M
-                if(j == chosenTask(i,1))
-                    gain = productivity(i,j) / production(chosenTask(i,1));
-                else
-                    gain = productivity(i,j) / (production(1,j)+productivity(i,j));
+        if(alive(i,1))
+            if(rand() < switchPossibilityFrequency*dt)
+                maxGain=-1000;
+                bestTask=0;
+                for j=1:M
+                    if(j == chosenTask(i,1))
+                        gain = productivity(i,j) / production(chosenTask(i,1));
+                    else
+                        gain = productivity(i,j) / (production(1,j)+productivity(i,j));
+                    end
+                    gain = gain - boredom(i,j);
+                    if(gain > maxGain)
+                        maxGain=gain;
+                        bestTask=j;
+                    end
                 end
-                gain = gain - boredom(i,j);
-                if(gain > maxGain)
-                    maxGain=gain;
-                    bestTask=j;
-                end
+                chosenTask(i,1) = bestTask;
             end
-            chosenTask(i,1) = bestTask;
         end
     end
     
     %Update boredom
     for i=1:N
-        boredomAtChosenTask(i,1) = boredom(i,chosenTask(i,1));
-        productivityAtChosenTask(i,1) = productivity(i,chosenTask(i,1));
+        if(alive(i,1))
+            boredomAtChosenTask(i,1) = boredom(i,chosenTask(i,1));
+            productivityAtChosenTask(i,1) = productivity(i,chosenTask(i,1));
+        end
     end
+    
+    %Kill someone
+    if(catastropheTime < t*dt)
+        if catastrophe
+            chosenTask(1,1) = -1;
+            alive(1,1) = 0;
+            totalmoney(1,1)=-1000;
+            boredom(1,:)=-10;
+            boredomAtChosenTask(1,1)=-10;
+            productivityAtChosenTask(1,1)=-10;
+            productivity(1,:)=-10;
+            money(1,1)=-10;
+        end
+    end
+    
     
     taskTime = [taskTime chosenTask];
     moneyTime = [moneyTime money];
@@ -164,6 +202,7 @@ title('Total production')
 
 subplot(4,2,4)
 plot(time, totalmoneyTime)
+axis([0 timesteps 0 max(totalmoneyTime(:))*1.1])
 title('Total money earned')
 
 %Idea for alternative representation (DIFFICULT!!)
